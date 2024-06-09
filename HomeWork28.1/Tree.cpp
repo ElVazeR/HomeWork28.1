b201d3f17123d601d080ead1aa92a186108612d2
ZED-2 <ZED-2@ELVAZER>
2024-06-09 21:32:28 +0500

    Добавьте файлы проекта.

---
+++ HomeWork28.1/Tree.cpp
@@ -0,0 +1,17 @@
+#include "Tree.hpp"
+#include <iostream>
+
+void print_tree(const Tree& T) {
+	std::cout << "?????: " << T.fruit << '\n';
+	std::cout << "???-?? ?????? ?? ??????: " << T.fruits_number << '\n';
+	std::cout << "???? ?? ???? ????: " << T.fruits << '\n';
+}
+int full_price(const Tree& T) {
+	return T.fruits_number * T.fruits;
+}
+
+int collect(const Tree& T, int num) {
+	if (num > T.fruits_number || num <= 0)
+		return 1;
+	return T.fruits * num;
+}
